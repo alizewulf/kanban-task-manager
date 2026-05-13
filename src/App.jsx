906692efedb08bb147b18f304a5102c1dac12d20
alizewulf <78692843+alizewulf@users.noreply.github.com>
2026-05-13 13:18:12 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './index.css'
+import SideBar from './Sidebar'
+import Header from './Header'
+import MainContent from './MainContent'
+export default function App() {
+  const [currentPage, setCurrentPage] = useState("Platform Launch")
+
+  return (
+    <div className="flex h-screen">
+      <SideBar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      
+      <div className="flex flex-col flex-1">
+        <Header title={currentPage} />
+        <MainContent />
+      </div>
+    </div>
+  )
+}
